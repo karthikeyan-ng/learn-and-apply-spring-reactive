fed2e9ee863c39bbc58ddb06fffbc10607c72f0e
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-18 22:28:31 +0530

    Initial Commit: Created Spring Boot Application with necessary dependencies

---
+++ src/main/java/com/techstack/react/LearnAndApplySpringReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.react;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnAndApplySpringReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnAndApplySpringReactiveApplication.class, args);
+	}
+
+}
