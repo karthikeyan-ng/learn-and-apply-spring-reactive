93d0e9cefbea30cd6af2540de23ecf45eb7858f5
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-18 23:42:34 +0530

    Usage of Flux.fromIterable and verify

---
+++ src/test/java/com/techstack/react/controller/FluxAndMonoFactoryTest.java
@@ -0,0 +1,31 @@
+package com.techstack.react.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxAndMonoFactoryTest {
+
+    List<String> names = Arrays.asList("Karthi", "Sara", "Pascal", "Thomas", "Christof");
+
+    @BeforeEach
+    void setup() {
+
+    }
+
+    @Test
+    @DisplayName("Test to use Flux.fromIterable")
+    void fluxUsingIterable() {
+        Flux<String> namesFlux = Flux.fromIterable(names);
+
+        StepVerifier.create(namesFlux)
+                    .expectNext("Karthi", "Sara", "Pascal", "Thomas", "Christof")
+                    .verifyComplete();
+    }
+
+}
