78fa4301e2d3b8818fb821a8ed8292eace02d412
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-18 22:37:51 +0530

    Test Flux: Subscribe with Error

--- src/test/java/com/techstack/react/controller/FluxAndMonoTestControllerTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoTestControllerTest.java
@@ -22,4 +22,22 @@ class FluxAndMonoTestControllerTest {
         stringFlux.subscribe(System.out::println);
     }
 
+    @Test
+    @DisplayName("This is Simple Flex to subscribe series of String with Exception by applying log")
+    void fluxTest1() {
+
+        Flux<String> stringFlux =
+                //Step1: Create
+                Flux.just("Spring Boot ", "Spring Data ", "Spring Message ")
+                    //Step2: Attach an Exception
+                    .concatWith(Flux.error(new RuntimeException("Exception while processing")))
+                    //Step6: Add log method to get detailed processing steps
+                    .log();
+
+        //Step3: Consume
+        stringFlux.subscribe(
+                System.out::println, //Step4: Data part
+                (e) -> System.err.println("Exception is " + e)  //Step5: Error part
+        );
+    }
 }
