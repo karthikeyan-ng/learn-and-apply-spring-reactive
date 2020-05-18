c0e6f7ed87262d73d8a66fdaf3dd6623a2edb94f
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-18 22:40:11 +0530

    Test Flux: This test case is to check whether Flux is sending data after Error event called?

--- src/test/java/com/techstack/react/controller/FluxAndMonoTestControllerTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoTestControllerTest.java
@@ -40,4 +40,34 @@ class FluxAndMonoTestControllerTest {
                 (e) -> System.err.println("Exception is " + e)  //Step5: Error part
         );
     }
+
+    @Test
+    @DisplayName("This is Simple Flex Test to subscribe series of String with Exception by applying log" +
+            " adding another concatWith to check after the onError event emitted")
+    void fluxTest2() {
+
+        Flux<String> stringFlux =
+                //Step1: Create
+                Flux.just("Spring Boot ", "Spring Data ", "Spring Message ")
+
+                        //Step2: Attach an Exception
+                        .concatWith(Flux.error(new RuntimeException("Exception while processing")))
+
+                        //Step7: To check whether Flux returns any data after exception?
+                        .concatWith(Flux.just("After Error"))
+
+                        //Step6: Add log method to get detailed processing steps
+                        .log();
+
+        //Step3: Consume
+        stringFlux.subscribe(
+                System.out::println, //Step4: Data part
+
+                (e) -> System.err.println("Exception is " + e), //Step5: Error part
+
+                //Step8: This Block of information will be executed for onComplete() status.
+                //This will not print if any error occurs
+                () -> System.out.println("Completed!")
+        );
+    }
 }
