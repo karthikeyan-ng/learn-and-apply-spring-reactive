a5a7debb5b05968cf7331a1ea9598f423dfe4ddc
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-21 15:46:11 +0530

    FluxAndMonoFilterTest by applying filter() method on Flux values. Filter will use Java 8 predicate.

---
+++ src/test/java/com/techstack/react/controller/FluxAndMonoFilterTest.java
@@ -0,0 +1,36 @@
+package com.techstack.react.controller;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FluxAndMonoFilterTest {
+
+    List<String> names = Arrays.asList("Karthi", "Sara", "Pascal", "Thomas", "Christof");
+
+    @Test
+    void filterTest() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                 .filter(s -> s.charAt(1) == 'a')
+                 .log();  // Karthi, Sara, Pascal
+
+        StepVerifier.create(namesFlux)
+                    .expectNext("Karthi", "Sara", "Pascal")
+                    .verifyComplete();
+    }
+
+    @Test
+    void filterLengthTest() {
+        Flux<String> namesFlux = Flux.fromIterable(names)
+                .filter(s -> s.length() > 4)
+                .log();  // Karthi, Pascal, Thomas, Christof
+
+        StepVerifier.create(namesFlux)
+                .expectNext("Karthi",  "Pascal", "Thomas", "Christof")
+                .verifyComplete();
+    }
+
+}
