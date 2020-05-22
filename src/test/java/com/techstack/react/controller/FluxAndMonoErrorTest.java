4867ad47b3fb4674f591d509098efd483bbcc1c9
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-22 17:25:45 +0530

    Catch the RuntimeException and Convert it to application Custom Exception. Using onErrorMap() method.

--- src/test/java/com/techstack/react/controller/FluxAndMonoErrorTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoErrorTest.java
@@ -1,5 +1,7 @@
 package com.techstack.react.controller;
 
+import com.techstack.react.controller.exception.CustomException;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
@@ -45,5 +47,21 @@ public class FluxAndMonoErrorTest {
                 .expectNext("A", "B", "C")
                 .expectNext("default")
                 .verifyComplete();
+    }
+
+    @Test
+    @DisplayName("Catch the RuntimeException and Convert it to application Custom Exception")
+    void fluxErrorHandling_OnErrorMap() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+                .onErrorMap(e -> new CustomException(e));
+
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+                .expectError(CustomException.class)
+                .verify();
     }
 }
---
+++ src/test/java/com/techstack/react/controller/exception/CustomException.java
@@ -0,0 +1,19 @@
+package com.techstack.react.controller.exception;
+
+public class CustomException extends Throwable {
+
+    private String message;
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public CustomException(Throwable e) {
+        this.message = e.getMessage();
+    }
+}
