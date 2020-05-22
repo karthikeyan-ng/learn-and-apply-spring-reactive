2d529da5da8885e6a669adb82be181727d0e5775
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-22 12:02:55 +0530

    FluxAndMonoTransformTest by applying window() method and subscribeOn(parallel()) process for Flux values. This example talks about parallel execution.

--- src/test/java/com/techstack/react/controller/FluxAndMonoTransformTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoTransformTest.java
@@ -8,6 +8,8 @@ import reactor.test.StepVerifier;
 import java.util.Arrays;
 import java.util.List;
 
+import static reactor.core.scheduler.Schedulers.parallel;
+
 public class FluxAndMonoTransformTest {
 
     List<String> names = Arrays.asList("Karthi", "Sara", "Pascal", "Thomas", "Christof");
@@ -77,6 +79,34 @@ public class FluxAndMonoTransformTest {
         StepVerifier.create(alphabetsFlux)
                     .expectNextCount(12)
                     .verifyComplete();
+    }
+
+    /**
+     * This will use the parallel-N thread to do your operation.
+     * The final output is not garnette.
+     * If you want to stick with your input order there are some other handy methods available.
+     */
+    @Test
+    @DisplayName("Execute FlatMap in Parallel")
+    void transformUsingFlatMap_Parallel() {
+        Flux<String> alphabetsFlux =
+                Flux.fromIterable(List.of("A", "B", "C", "D", "E", "F"))
+
+                        //Step1: Here window will wait to pass two elements. Flux<Flux<String>> -> (A,B), (C,D), (E,F)
+                        .window(2)
+
+                        //When to use flatMap? If you want to call a DB or external service that returns a Flux<T>
+                        .flatMap(s -> s.map(this::convertToList)
+                                //Step2: subscribe elements in parallel()
+                                .subscribeOn(parallel()))
+
+                        //Step3: Again convert back to Flux<String> using FlatMap
+                        .flatMap(s -> Flux.fromIterable(s))
+                        .log();
+
+        StepVerifier.create(alphabetsFlux)
+                .expectNextCount(12)
+                .verifyComplete();
     }
 
     private List<String> convertToList(String s) {
