78c679d77d78c5726a37558180af76532c8a44de
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-22 18:26:49 +0530

    Finite Sequence Flux using take() method

--- src/test/java/com/techstack/react/controller/FluxAndMonoWithTimeTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoWithTimeTest.java
@@ -2,6 +2,7 @@ package com.techstack.react.controller;
 
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
 
 import java.time.Duration;
 
@@ -12,7 +13,7 @@ public class FluxAndMonoWithTimeTest {
 
         /**
          * The below code will start emitting values from 0 to N (infinite) long values.
-         * Each value will be emitting each 200 milli seconds
+         * Each value will be emitting each 100 milli seconds
          */
         Flux<Long> infiniteFlux = Flux.interval(Duration.ofMillis(100))
                 .log();
@@ -23,5 +24,22 @@ public class FluxAndMonoWithTimeTest {
         //If you comment this line, nothing will be printed on console output.
         //Values will be start emitting till 3000 milli seconds
         Thread.sleep(3000);
+    }
+
+    @Test
+    void finiteSequenceTest() throws InterruptedException {
+
+        /**
+         * The below code will start emitting values from 0 to N using take(N) long values.
+         * Each value will be emitting each 100 milli seconds
+         */
+        Flux<Long> finiteFlux = Flux.interval(Duration.ofMillis(100))
+                                    .take(3)
+                                    .log();
+
+        StepVerifier.create(finiteFlux)
+                    .expectSubscription()
+                    .expectNext(0L, 1L, 2L)
+                    .verifyComplete();
     }
 }
