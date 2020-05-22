fe9b924ac31b12de732afb5d3f3c3886a98894f0
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-22 13:00:29 +0530

    How to combine two Flux<T> using merge() with delayElements()

--- src/test/java/com/techstack/react/controller/FluxAndMonoCombineTest.java
+++ src/test/java/com/techstack/react/controller/FluxAndMonoCombineTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
 
+import java.time.Duration;
+
 public class FluxAndMonoCombineTest {
 
     @Test
@@ -22,5 +24,24 @@ public class FluxAndMonoCombineTest {
                     .expectSubscription()
                     .expectNext("A", "B", "C", "D", "E", "F")
                     .verifyComplete();
+    }
+
+    @Test
+    void combineUsingMerge_withDelay() {
+        Flux<String> flux1 = Flux.just("A", "B", "C").delayElements(Duration.ofSeconds(1));
+        Flux<String> flux2 = Flux.just("D", "E", "F").delayElements(Duration.ofSeconds(1));
+
+        /**
+         * In real use case, if you want to do two DB / External API calls and
+         * combine them into another Flux and send it back to the caller
+         * you can use Flux.merge()
+         */
+
+        Flux<String> mergedFlux = Flux.merge(flux1, flux2);
+
+        StepVerifier.create(mergedFlux.log())
+                .expectSubscription()
+                .expectNextCount(6)
+                .verifyComplete();
     }
 }
