801f088a5b5b4cd069020ff45749e08429e4ea15
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-22 17:15:44 +0530

    How to handle error scenario and handle it using onErrorResume() method

---
+++ src/test/java/com/techstack/react/controller/FluxAndMonoErrorTest.java
@@ -0,0 +1,34 @@
+package com.techstack.react.controller;
+
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+public class FluxAndMonoErrorTest {
+
+    @Test
+    void fluxErrorHandling() {
+        Flux<String> stringFlux = Flux.just("A", "B", "C")
+                .concatWith(Flux.error(new RuntimeException("Exception Occurred")))
+                .concatWith(Flux.just("D"))
+
+                //How to handle an Error in a Reactive stream
+                //This block will exeucte any Error
+                .onErrorResume(e -> {
+                    System.out.println("Exception is : " + e);
+                    return Flux.just("default", "default1");
+                });
+
+        StepVerifier.create(stringFlux.log())
+                .expectSubscription()
+                .expectNext("A", "B", "C")
+
+                //Scenario 1: Error Scenario
+//                .expectError(RuntimeException.class)
+//                .verify();
+
+                //Scenario 2: Handled Error Scenario with default response
+                .expectNext("default", "default1")
+                .verifyComplete();
+    }
+}
