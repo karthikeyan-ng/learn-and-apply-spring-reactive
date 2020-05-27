470bbc46d337a21df291b9dc7c35fbe3f5cb8873
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-27 12:59:24 +0530

    Items Client: Initial commit

---
+++ items-client/src/main/java/com/techstack/reactive/client/ItemsClientApplication.java
@@ -0,0 +1,13 @@
+package com.techstack.reactive.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItemsClientApplication.class, args);
+	}
+
+}
---
+++ items-client/src/test/java/com/techstack/reactive/client/ItemsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techstack.reactive.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItemsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
