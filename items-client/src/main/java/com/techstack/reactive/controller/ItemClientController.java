0457cde03adb92ee74da0225df7fa08b00f7ea7c
Karthikeyan Nithiyanandam <karthikeyan.ng@gmail.com>
2020-05-27 19:16:56 +0530

    Items Client: Created a GET mapping call to connect to Items-Service to get the data using "exchange()" method.

--- items-client/src/main/java/com/techstack/reactive/controller/ItemClientController.java
+++ items-client/src/main/java/com/techstack/reactive/controller/ItemClientController.java
@@ -11,6 +11,11 @@ public class ItemClientController {
 
     WebClient webClient = WebClient.create("http://localhost:8080");
 
+    /**
+     * Here retrieve() method will give access to the direct response content
+     *
+     * @return
+     */
     @GetMapping("/client/retrieve")
     public Flux<Item> getAllItemsUsingRetrieve() {
 
@@ -19,6 +24,21 @@ public class ItemClientController {
                 .uri("/v1/items")
                 .retrieve()
                 .bodyToFlux(Item.class)
-                .log("Items in Client Call");
+                .log("Items in Client Call using Retrieve");
+    }
+
+    /**
+     * Here exchange() method will give access to the RAW ClientResponse content
+     * @return
+     */
+    @GetMapping("/client/exchange")
+    public Flux<Item> getAllItemsUsingExchange() {
+
+        return webClient
+                .get()
+                .uri("/v1/items")
+                .exchange()
+                .flatMapMany(clientResponse -> clientResponse.bodyToFlux(Item.class))
+                .log("Items in Client Call using Exchange");
     }
 }
